4bf9cbafa71ecffec68747b028c2084618e1fca7
Sandeep Negi <snegi@joriehp.com>
2024-04-23 20:31:54 +0530

    commit

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -301,10 +301,56 @@ const UpdateUserCoverImage = asyncHandler(async (req, res) => {
 });
 
 const GetUserChannelProfile = asyncHandler(async (req, res) => {
-  const {username} = req?.params;
-  if(!username?.trim()){
-    throw new ApiError(400,"Username is missing.");
+  const { username } = req?.params;
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing.");
+  }
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribeTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: { $size: "$subscribers" },
+        channelsSubscribedToCount: { $size: "$subscribeTo" },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req?.user?.id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+      },
+    },
+  ]);
 
+  if (!channel) {
   }
 });
 export {
