3633e5e891df8d494beab6919e57956273b73833
sandeep0231 <161934071+sandeep0231@users.noreply.github.com>
2024-04-24 20:53:39 +0530

    routes for format

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { LoginUser, LogoutUser, RegisterUser, RefreshAccessToken, ChangeCurrentPassword, getCurrentUser, updateAccountDetails, UpdateUserAvatar, UpdateUserCoverImage, GetUserChannelProfile, getWatchHistory } from "../controller/user.controller.js";
+import {
+  LoginUser,
+  LogoutUser,
+  RegisterUser,
+  RefreshAccessToken,
+  ChangeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  UpdateUserAvatar,
+  UpdateUserCoverImage,
+  GetUserChannelProfile,
+  getWatchHistory,
+} from "../controller/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -23,9 +35,15 @@ router.route("/logout").post(verifyJWT, LogoutUser);
 router.route("/refresh_token").post(RefreshAccessToken);
 router.route("/changeCurrent_password").post(verifyJWT, ChangeCurrentPassword);
 router.route("/getCurrent_user").get(verifyJWT, getCurrentUser);
-router.route("/user_accountInfoUpdate").patch(verifyJWT,updateAccountDetails);
-router.route("/updateUser_avatar").patch(verifyJWT,upload.single("avatar"),UpdateUserAvatar);
-router.route("/update_coverImage").patch(verifyJWT,upload.single("coverimage"),UpdateUserCoverImage);
-router.route("/userChannel_profile/:username").get(verifyJWT,GetUserChannelProfile);
-router.route("/watichhistory").get(verifyJWT,getWatchHistory);
+router.route("/user_accountInfoUpdate").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/updateUser_avatar")
+  .patch(verifyJWT, upload.single("avatar"), UpdateUserAvatar);
+router
+  .route("/update_coverImage")
+  .patch(verifyJWT, upload.single("coverimage"), UpdateUserCoverImage);
+router
+  .route("/userChannel_profile/:username")
+  .get(verifyJWT, GetUserChannelProfile);
+router.route("/watichhistory").get(verifyJWT, getWatchHistory);
 export default router;
